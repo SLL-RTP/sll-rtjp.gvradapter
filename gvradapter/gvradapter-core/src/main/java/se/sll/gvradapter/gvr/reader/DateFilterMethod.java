e065ed04ba8730681d6f76e4dcc93398c0b76cb6
Eric Jacobsson <eric.jacobsson@mawell.com>
2014-02-19 12:01:03 +0100

    Deleting an old file from the refactoring that didn't get properly moved.

--- gvradapter/gvradapter-core/src/main/java/se/sll/gvradapter/gvr/reader/DateFilterMethod.java
+++
@@ -1,7 +0,0 @@
-package se.sll.gvradapter.gvr.reader;
-
-/** Used as a parameter object in {@link se.sll.gvradapter.gvr.reader.GVRFileReader}. */
-public enum DateFilterMethod {
-    FILENAME,
-    METADATA
-}
