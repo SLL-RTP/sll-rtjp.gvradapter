52458226ac6a39feb1c6ef1297f88f85706b7969
Eric Jacobsson <eric.jacobsson@mawell.com>
2014-06-17 11:30:58 +0200

    Further cleanups. Modified the OIDList class to contains methods for getting the name and OID for a specific code system. Added a CodeSystem enum with all the code system names for the OIDList mapper.

--- reimbursementadapter/getadmincareevent-core/src/main/java/se/sll/reimbursementadapter/gvr/transform/OIDList.java
+++ reimbursementadapter/getadmincareevent-core/src/main/java/se/sll/reimbursementadapter/gvr/transform/OIDList.java
@@ -1,36 +1,95 @@
+/**
+ *  Copyright (c) 2013 SLL <http://sll.se/>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package se.sll.reimbursementadapter.gvr.transform;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Contains a list of mappings for all the types of code systems to their respective OID:s and CV Names.
- * The OID can be found in OID_[name], and the text can be found in OID_[name]_TEXT.
  */
 public class OIDList {
 
-    // Kodverk
-    protected static final String OID_TEMPORARY_PATIENT_ID = "1.2.752.97.3.1.3";
-    protected static final String OID_COORDINATION_ID = "1.2.752.129.2.1.3.3";
-    protected static final String OID_PATIENT_IDENTIFIER = "1.2.752.129.2.1.3.1";
-    protected static final String OID_HYBRID_GUID_IDENTIFIER = "1.2.752.129.2.1.2.1";
-    protected static final String OID_ICD10_SE = "1.2.752.116.1.1.1.1.3";
-    protected static final String OID_ICD10_SE_TEXT = "Internationell statistisk klassifikation av sjukdomar och relaterade hälsoproblem, systematisk förteckning (ICD-10-SE)";
-    protected static final String OID_KVÅ = "1.2.752.116.1.3.2.1.4";
-    protected static final String OID_ATC = "1.2.752.129.2.2.3.1.1";
-    // Formella kodverk enligt V-TIM 2.0.
-    protected static final String OID_KV_KÖN = "1.2.752.129.2.2.1.1";
-    protected static final String OID_KV_KONTAKTTYP = "1.2.752.129.2.2.2.25";
-    protected static final String OID_KV_KONTAKTTYP_TEXT = "KV kontakttyp";
-    protected static final String OID_KV_LÄN = "1.2.752.129.2.2.1.18";
-    protected static final String OID_KV_LÄN_TEXT = "KV_LÄN - Länskod enligt SCB";
-    protected static final String OID_KV_KOMMUN = "1.2.752.129.2.2.1.17";
-    protected static final String OID_KV_KOMMUN_TEXT = "KV_KOMMUN - Kommunkod enligt SCB";
-    protected static final String OID_KV_FÖRSAMLING = "1.2.752.129.2.2.1.16";
-    protected static final String OID_KV_FÖRSAMLING_TEXT = "KV_FÖRSAMLING - Församlingskod enligt SCB";
-    // Egna kodverk skapade från Codeserver.
-    protected static final String OID_SLL_CS_UPPDRAGSTYP = "SLL.CS.UPPDRAGSTYP";
-    protected static final String OID_SLL_CS_UPPDRADSTYP_TEXT = "SLL Code Server definition from the 'UPPDRAGSTYP' table.";
-    protected static final String OID_SLL_CS_TILLSTAND = "SLL.CS.TILLSTAND";
-    protected static final String OID_SLL_CS_TILLSTAND_TEXT = "SLL Code Server definition from the 'UPPDRAGSTYP' table";
-    protected static final String OID_SLL_CS_VDG = "SLL.CS.VDG";
-    protected static final String OID_SLL_CS_VDG_TEXT = "SLL Code Server definition from the 'VDG' table";
+    private static Map<CodeSystem, String> oidMap = new HashMap<>();
+    private static Map<CodeSystem, String> nameMap = new HashMap<>();
 
+    static {
+        oidMap.put(CodeSystem.PERSONNUMMER, "1.2.752.129.2.1.3.1");
+        nameMap.put(CodeSystem.PERSONNUMMER, "Personnummer - vid användning inom vård och omsorg");
+
+        oidMap.put(CodeSystem.SAMORDNINGSNUMMER, "1.2.752.129.2.1.3.3");
+        nameMap.put(CodeSystem.SAMORDNINGSNUMMER, "Samordningsnummer - vid användning inom vård och omsorg");
+
+        oidMap.put(CodeSystem.RESERVNUMMER_SLL, "1.2.752.97.3.1.3");
+        nameMap.put(CodeSystem.RESERVNUMMER_SLL, "Lokala reservnummer i SLL");
+
+        oidMap.put(CodeSystem.HYBRID_GUID_IDENTIFIER,"1.2.752.129.2.1.2.1");
+        nameMap.put(CodeSystem.HYBRID_GUID_IDENTIFIER, "Hybrididentifierare bestående av HSA-id för org. där id är unikt + id");
+
+        oidMap.put(CodeSystem.ICD10_SE, "1.2.752.116.1.1.1.1.3");
+        nameMap.put(CodeSystem.ICD10_SE, "Internationell statistisk klassifikation av sjukdomar och relaterade hälsoproblem, systematisk förteckning (ICD-10-SE)");
+
+        oidMap.put(CodeSystem.KVÅ, "1.2.752.116.1.3.2.1.4");
+        nameMap.put(CodeSystem.KVÅ, "Klassifikation av vårdåtgärder (KVÅ)");
+
+        oidMap.put(CodeSystem.ATC, "1.2.752.129.2.2.3.1.1");
+        nameMap.put(CodeSystem.ATC, "Anatomical Therapeutic Chemical classification system (ATC)");
+
+        oidMap.put(CodeSystem.KV_KÖN, "1.2.752.129.2.2.1.1");
+        nameMap.put(CodeSystem.KV_KÖN, "kv_kön Anger adminstrativt kön");
+
+        oidMap.put(CodeSystem.KV_KONTAKTTYP, "1.2.752.129.2.2.2.25");
+        nameMap.put(CodeSystem.KV_KONTAKTTYP, "kv_kontakttyp, anger vilken typ kontakten är");
+
+        oidMap.put(CodeSystem.KV_LÄN, "1.2.752.129.2.2.1.18");
+        nameMap.put(CodeSystem.KV_LÄN, "kv_län, länskod enligt SCB");
+
+        oidMap.put(CodeSystem.KV_KOMMUN, "1.2.752.129.2.2.1.17");
+        nameMap.put(CodeSystem.KV_KOMMUN, "kv_kommun, kommunkod enligt SCB");
+
+        oidMap.put(CodeSystem.KV_FÖRSAMLING, "1.2.752.129.2.2.1.16");
+        nameMap.put(CodeSystem.KV_FÖRSAMLING, "KV_FÖRSAMLING - Församlingskod enligt SCB");
+
+        oidMap.put(CodeSystem.SLL_CS_UPPDRAGSTYP, "SLL.CS.UPPDRAGSTYP");
+        nameMap.put(CodeSystem.SLL_CS_UPPDRAGSTYP, "SLL Code Server definition from the 'UPPDRAGSTYP' table.");
+
+        oidMap.put(CodeSystem.SLL_CS_TILLSTÅND, "SLL.CS.TILLSTAND");
+        nameMap.put(CodeSystem.SLL_CS_TILLSTÅND, "SLL Code Server definition from the 'TILLSTAND' table");
+
+        oidMap.put(CodeSystem.SLL_CS_VDG, "SLL.CS.VDG");
+        nameMap.put(CodeSystem.SLL_CS_VDG, "SLL Code Server definition from the 'VDG' table");
+    }
+
+    /**
+     * Gets the OID for a specific incoming {@link CodeSystem}
+     *
+     * @param cs The Code System to map from.
+     * @return The mapped OID for the code system.
+     */
+    public static String getOid(CodeSystem cs) {
+        return oidMap.get(cs);
+    }
+
+    /**
+     * Gets the Name for a specific incoming {@link CodeSystem}
+     *
+     * @param cs The Code System to map from.
+     * @return The mapped OID for the code system.
+     */
+    public static String getName(CodeSystem cs) {
+        return nameMap.get(cs);
+    }
 }
